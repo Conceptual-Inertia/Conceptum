512aaeea5e4cc4fcdb388d5552214830dc15d35f
Alex Fang <frjalex@temple.edu>
2016-10-12 18:08:56 -0400

    sample assembler

--- src/main.c
+++ src/main.c
@@ -175,9 +175,17 @@ struct ConceptProgram_t {
     int len;
 } concept_program;
 
+typedef struct {
+    int32_t instr;
+    void *payload;
+} ConceptInstruction_t;
+
 BOOL is_running;
 
 void **symbols_list;
+
+char **procedure_call_table;
+static int procedure_call_table_length;
 
 /*
  * Stack Operations Functions
@@ -788,76 +796,6 @@ int32_t concept_debug() {
  * File Reader Utilities and Lexer
  */
 
-
-void read_prog(char *file_path) {
-    int lines_allocated = 128;
-    int max_line_len = 100;
-
-    /* Allocate lines of text */
-    char **words = (char **)rmalloc(sizeof(char*)*lines_allocated);
-    if (words==NULL) {
-        fprintf(stderr,"Out of memory (1).\n");
-        exit(1);
-    }
-
-    FILE *fp = fopen(file_path, "r");
-    if (fp == NULL) {
-        fprintf(stderr,"Error opening file.\n");
-        exit(2);
-    }
-
-    int i;
-    for (i=0;1;i++) {
-        int j;
-
-        /* Have we gone over our line allocation? */
-        if (i >= lines_allocated) {
-            int new_size;
-
-            /* Double our allocation and re-allocate */
-            new_size = lines_allocated*2;
-            words = (char **)rrealloc(words,sizeof(char*)*new_size);
-            if (words==NULL) {
-                fprintf(stderr,"err read_file(): Out of memory.\n");
-                exit(3);
-            }
-            lines_allocated = new_size;
-        }
-        /* Allocate space for the next line */
-        words[i] = rmalloc(max_line_len);
-        if (words[i]==NULL) {
-            fprintf(stderr,"err read_file(): Out of memory (3).\n");
-            exit(4);
-        }
-        if (fgets(words[i],max_line_len-1,fp)==NULL)
-            break;
-
-        /* Get rid of CR or LF at end of line */
-        for (j=strlen(words[i])-1;j>=0 && (words[i][j]=='\n' || words[i][j]=='\r');j--);
-        ; ; ;
-        words[i][j+1]='\0';
-    }
-    /* Close file */
-    fclose(fp);
-
-    concept_program.code = words;
-    concept_program.len = i;
-
-    //int j;
-    //for(j = 0; j < i; j++)
-    //    printf("%s\n", words[j]);
-
-    /* Good practice to free memory */
-    //for (;i>=0;i--)
-    //    free(words[i]);
-    //free(words);
-    //return 0;
-}
-
-ConceptBytecode_t* procedure_call_lookup(char *procedure_name) {
-    // TODO
-}
-
 // Iterating event loop
 // TODO implement iterator
 void* event_loop(ConceptBytecode_t cbp[], ConceptStack_t *stack, ConceptStack_t *global_stack) { // TODO
@@ -945,8 +883,8 @@ void* event_loop(ConceptBytecode_t cbp[], ConceptStack_t *stack, ConceptStack_t 
                 stack_push(global_stack, stack_pop(stack));
             case CONCEPT_CALL:
                 stack_alloc(&call_stack, CONCEPTFP_MAX_LENGTH);
-                void *ret_val = event_loop(procedure_call_lookup((char *) (cbp->value)), &call_stack, global_stack);
-                stack_push(stack, ret_val);
+                // TODO void *ret_val = event_loop(/*todo*/), &call_stack, global_stack);
+                // stack_push(stack, ret_val);
             case CONCEPT_RETURN:
                 break;
             default:
@@ -963,70 +901,270 @@ void cleanup(ConceptStack_t *global_stack) {
     stack_free(global_stack);
 }
 
-// parse() called after initializing concept_program global val
-ConceptBytecode_t ** parse(){
-    ConceptBytecode_t* bytecodes = malloc(sizeof(ConceptBytecode_t) * (concept_program.len));
-    for(int i = 0; i < concept_program.len; i++) {
-        int counter;
-        for(counter = 0; counter < strlen(concept_program.code[i]); counter++) {
-            if(concept_program.code[i][counter] == '\0' || concept_program.code[i][counter] == ' ' || concept_program.code[i][counter] == '\t')
-                break;
-        }
 
-        char *instr = rmalloc(sizeof(char) * counter);
-
-        for(int j = 0; j < strlen(concept_program.code[i]); j++) {
-            if(concept_program.code[i][j] != '\0' && concept_program.code[i][j] != ' ' && concept_program.code[i][j] != '\t')
-                instr[j] = concept_program.code[i][j];
-        }
-
-        // The advent of a gigantic if... C switches doesn't support char*
-        if(!strcmp(instr, "iadd")) bytecodes[i].instruction = CONCEPT_IADD;
-        else if(!strcmp(instr, "idiv")) bytecodes[i].instruction = CONCEPT_IDIV;
-        else if(!strcmp(instr, "imul")) bytecodes[i].instruction = CONCEPT_IMUL;
-        else if(!strcmp(instr, "fadd")) bytecodes[i].instruction = CONCEPT_FADD;
-        else if(!strcmp(instr, "fdiv")) bytecodes[i].instruction = CONCEPT_FDIV;
-        else if(!strcmp(instr, "fmul")) bytecodes[i].instruction = CONCEPT_FMUL;
-        else if(!strcmp(instr, "ilt")) bytecodes[i].instruction = CONCEPT_ILT;
-        else if(!strcmp(instr, "ieq")) bytecodes[i].instruction = CONCEPT_IEQ;
-        else if(!strcmp(instr, "igt")) bytecodes[i].instruction = CONCEPT_IGT;
-        else if(!strcmp(instr, "flt")) bytecodes[i].instruction = CONCEPT_FLT;
-        else if(!strcmp(instr, "feq")) bytecodes[i].instruction = CONCEPT_FEQ;
-        else if(!strcmp(instr, "fgt")) bytecodes[i].instruction = CONCEPT_FGT;
-        else if(!strcmp(instr, "and")) bytecodes[i].instruction = CONCEPT_AND;
-        else if(!strcmp(instr, "or")) bytecodes[i].instruction = CONCEPT_OR;
-        else if(!strcmp(instr, "xor")) bytecodes[i].instruction = CONCEPT_XOR;
-        else if(!strcmp(instr, "ne")) bytecodes[i].instruction = CONCEPT_NE;
-        else if(!strcmp(instr, "if")) bytecodes[i].instruction = CONCEPT_IF;
-        else if(!strcmp(instr, "cconst")) bytecodes[i].instruction = CONCEPT_CCONST;
-        else if(!strcmp(instr, "iconst")) bytecodes[i].instruction = CONCEPT_ICONST;
-        else if(!strcmp(instr, "sconst")) bytecodes[i].instruction = CONCEPT_SCONST;
-        else if(!strcmp(instr, "fconst")) bytecodes[i].instruction = CONCEPT_FCONST;
-        else if(!strcmp(instr, "bconst")) bytecodes[i].instruction = CONCEPT_BCONST;
-        else if(!strcmp(instr, "vconst")) bytecodes[i].instruction = CONCEPT_VCONST;
-        else if(!strcmp(instr, "print")) bytecodes[i].instruction = CONCEPT_PRINT;
-        else if(!strcmp(instr, "pop")) bytecodes[i].instruction = CONCEPT_POP;
-        else if(!strcmp(instr, "goto")) bytecodes[i].instruction = CONCEPT_GOTO;
-        else if(!strcmp(instr, "if_icmple")) bytecodes[i].instruction = CONCEPT_IF_ICMPLE;
-        else on_error(CONCEPT_COMPILER_ERROR, "Invalid instruction detected.", CONCEPT_STATE_CATASTROPHE, CONCEPT_ABORT);
-
-        char *str = concept_program.code[i];
-
-        char *param = substring(str, (counter + 1), (int)strlen(str));
-        bytecodes[i].value = param;
-    }
-    //return bytecodes;
-
-    // TODO parse functions
-    int procedures_count = 1; // TODO not feasible, must implement in THE LOOP
-    // Implementation Notes
-    // bytecode_groups shall directly be pointed to each group of bytecodes, serving in lieu of the bytecodes array
-    // The code below is simply a temporary solution for the sake of implementation of the Event Loop.
-    // Another struct needs to be constructed to store the function's name or address for the call cmd to look upon.
-    ConceptBytecode_t **bytecode_groups = (ConceptBytecode_t **)rmalloc(sizeof(ConceptBytecode_t *));
-    bytecode_groups[0] = bytecodes;
-    return bytecode_groups;
+char* substring(char *string, int start, int end) {
+    char* subbuff = malloc(sizeof(char) * (end - start));
+    memcpy(subbuff, &string[start], (end - start));
+    subbuff[end - start] = '\0';
+    return subbuff;
 }
+
+
+char* remove_spaces(char *src){
+    char *dst;
+    int s, d=0;
+    for (s=0; src[s] != 0; s++)
+        if (src[s] != ' ' && src[s] != '\t') {
+            dst[d] = src[s];
+            d++;
+        }
+    dst[d] = 0;
+    return dst;
+}
+
+void read_prog(char *file_path) {
+    int lines_allocated = 128;
+    int max_line_len = 100;
+
+    /* Allocate lines of text */
+    char **words = (char **)rmalloc(sizeof(char*)*lines_allocated);
+    if (words==NULL) {
+        fprintf(stderr,"Out of memory (1).\n");
+        exit(1);
+    }
+
+    FILE *fp = fopen(file_path, "r");
+    if (fp == NULL) {
+        fprintf(stderr,"Error opening file.\n");
+        exit(2);
+    }
+
+    int i;
+    for (i=0;1;i++) {
+        int j;
+
+        /* Have we gone over our line allocation? */
+        if (i >= lines_allocated) {
+            int new_size;
+
+            /* Double our allocation and re-allocate */
+            new_size = lines_allocated*2;
+            words = (char **)rrealloc(words,sizeof(char*)*new_size);
+            if (words==NULL) {
+                fprintf(stderr,"err read_file(): Out of memory.\n");
+                exit(3);
+            }
+            lines_allocated = new_size;
+        }
+        /* Allocate space for the next line */
+        words[i] = rmalloc(max_line_len);
+        if (words[i]==NULL) {
+            fprintf(stderr,"err read_file(): Out of memory (3).\n");
+            exit(4);
+        }
+        if (fgets(words[i],max_line_len-1,fp)==NULL)
+            break;
+
+        /* Get rid of CR or LF at end of line */
+        for (j=strlen(words[i])-1;j>=0 && (words[i][j]=='\n' || words[i][j]=='\r');j--);
+        ; ; ;
+        words[i][j+1]='\0';
+    }
+    /* Close file */
+    fclose(fp);
+
+    // words: string array
+    // Every string in the array represents a line in the actual file.
+    // The array itself represents the text file composed of multiple line(s).
+    // The length here, (i) means the length of the string array (since a pointer of pointer can not be measured)
+
+    concept_program.code = words;
+    concept_program.len = i;
+
+    //int j;
+    //for(j = 0; j < i; j++)
+    //    printf("%s\n", words[j]);
+
+    /* Good practice to free memory */
+    //for (;i>=0;i--)
+    //    free(words[i]);
+    //free(words);
+    //return 0;
+}
+// 1
+
+// parse_procedures() reads in line by line, and finds the line declaring a procedure.
+// After that the procedure is being parsed in to an array of linear bytecodes
+// After that a bytecode array is constructed
+// A similar array of strings ^^ procedure_call_table is also constructed in order to map the value of a function to its address in the bytecode
+// array
+// TODO: finally the interpreter will perform inline expansion on all calls to make the destination procedure's name NOT
+// the String name, but the ACTUAL address of the bytecode procedure, which in turn makes an O(n) + O(1) complexity an O(1) complexity
+ConceptInstruction_t** parse_procedures() {
+    int how_many_procedures = 0;
+    for(int d = 0; d < concept_program.len; d++) {
+        if(strstr(concept_program.code[d], "procedure")) {
+            how_many_procedures++;
+        }
+    }
+    procedure_call_table = (char **)rmalloc(sizeof(char *)*how_many_procedures);
+    int prog_counter = 0;
+    for(int d = 0; d < concept_program.len; d++) {
+        if(strstr(concept_program.code[d], "procedure")) {
+            char *proc = concept_program.code[d];
+            char *proc_w_s = remove_spaces(proc);
+            char *proc_name = substring(proc, 8, strlen(proc_w_s)-1);
+            procedure_call_table[prog_counter] = proc_name;
+            prog_counter++;
+        }
+    }
+
+    procedure_call_table_length = prog_counter;
+
+    ConceptInstruction_t **compiled_bytecode_collection = (ConceptInstruction_t **)rmalloc(sizeof(ConceptInstruction_t *)* prog_counter);
+    int procedure_counter = 0;
+    for(int j = 0; j < concept_program.len; j++) {
+        if(strstr(concept_program.code[j], "procedure")) {
+            ConceptInstruction_t *procedure; // ConceptInstruction_t
+            int counter = 0;
+            int i = j;
+            for(j; !strstr(concept_program.code[j], "ret"); j++);
+            int procedure_len = j - i;
+            char *prog_name_line = concept_program.code[i];
+
+
+            procedure = (ConceptInstruction_t *)rmalloc(procedure_len * sizeof(ConceptInstruction_t)); // including the return statement
+
+            for(i = i + 1; i <= j; i++) { // from the first line of program to the ret statement, read every line and parse
+                // parse, parse, parse!
+                char *s_line = concept_program.code[i];
+                int p;
+                for(p = 0; p < strlen(s_line) && s_line[p] != ' ' && s_line[p] != '\t'; p++);
+                char *instr = (char *)rmalloc(sizeof(char) * p);
+                for(int q = 0; q < p; q++) instr[q] = s_line[q];
+                int param_flag = 0;
+                char *param;
+                if(strlen(s_line) - p > 0) {
+                    param_flag = 1;
+                    param = (char *)rmalloc(sizeof(char)*(strlen(s_line)-p));
+                    int r = 0;
+                    for(p = p + 1; p < strlen(s_line); p++) {
+                        param[r] = s_line[p];
+                        r++;
+                    }
+                }
+
+                // The advent of a gigantic if... C switches doesn't support char*
+                if(!strcmp(instr, "iadd")) procedure[i].instr = CONCEPT_IADD;
+                else if(!strcmp(instr, "idiv")) procedure[i].instr = CONCEPT_IDIV;
+                else if(!strcmp(instr, "imul")) procedure[i].instr = CONCEPT_IMUL;
+                else if(!strcmp(instr, "fadd")) procedure[i].instr = CONCEPT_FADD;
+                else if(!strcmp(instr, "fdiv")) procedure[i].instr = CONCEPT_FDIV;
+                else if(!strcmp(instr, "fmul")) procedure[i].instr = CONCEPT_FMUL;
+                else if(!strcmp(instr, "ilt")) procedure[i].instr = CONCEPT_ILT;
+                else if(!strcmp(instr, "ieq")) procedure[i].instr = CONCEPT_IEQ;
+                else if(!strcmp(instr, "igt")) procedure[i].instr = CONCEPT_IGT;
+                else if(!strcmp(instr, "flt")) procedure[i].instr = CONCEPT_FLT;
+                else if(!strcmp(instr, "feq")) procedure[i].instr = CONCEPT_FEQ;
+                else if(!strcmp(instr, "fgt")) procedure[i].instr = CONCEPT_FGT;
+                else if(!strcmp(instr, "and")) procedure[i].instr = CONCEPT_AND;
+                else if(!strcmp(instr, "or")) procedure[i].instr = CONCEPT_OR;
+                else if(!strcmp(instr, "xor")) procedure[i].instr = CONCEPT_XOR;
+                else if(!strcmp(instr, "ne")) procedure[i].instr = CONCEPT_NE;
+                else if(!strcmp(instr, "if")) procedure[i].instr = CONCEPT_IF;
+                else if(!strcmp(instr, "cconst")) {
+                    procedure[i].instr = CONCEPT_CCONST;
+                    if(!param_flag) exit(130);
+                    char *c = rmalloc(sizeof(char));
+                    *c = param[0];
+                    procedure[i].payload = (void *)c;
+                } else if(!strcmp(instr, "iconst")) {
+                    procedure[i].instr = CONCEPT_ICONST;
+                    if(!param_flag) exit(130);
+                    int32_t *a = rmalloc(sizeof(int));
+                    *a = atoi(param);
+                    procedure[i].payload = (void *)a;
+                } else if(!strcmp(instr, "sconst")) {
+                    procedure[i].instr = CONCEPT_SCONST;
+                    if(!param_flag) exit(130);
+                    procedure[i].payload = (void *)param;
+                } else if(!strcmp(instr, "fconst")) {
+                    procedure[i].instr = CONCEPT_FCONST;
+                    if(!param_flag) exit(130);
+                    float *f = rmalloc(sizeof(float));
+                    *f = (float)atof(param);
+                    procedure[i].payload = (void *)f;
+                } else if(!strcmp(instr, "bconst")) {
+                    procedure[i].instr = CONCEPT_BCONST;
+                    if(!param_flag) exit(130);
+                    int32_t *b = rmalloc(sizeof(int32_t));
+                    *b = atoi(param);
+                    procedure[i].payload = (void *)b;
+                } else if(!strcmp(instr, "vconst")) {
+                    procedure[i].instr = CONCEPT_VCONST;
+                    // if(!param_flag) exit(130);
+                    // procedure[i].payload = (void *)void;
+                } else if(!strcmp(instr, "print")) {
+                    procedure[i].instr = CONCEPT_PRINT;
+                } else if(!strcmp(instr, "pop")) {
+                    procedure[i].instr = CONCEPT_POP;
+                } else if(!strcmp(instr, "goto")) {
+                    procedure[i].instr = CONCEPT_GOTO;
+                    if(!param_flag) exit(130);
+                    int32_t *goto_line_num = rmalloc(sizeof(int32_t));
+                    *goto_line_num = atoi(param);
+                    procedure[i].payload = goto_line_num;
+                } else if(!strcmp(instr, "if_icmple")) {
+                    procedure[i].instr = CONCEPT_IF_ICMPLE;
+                    if(!param_flag) exit(130);
+                    int32_t *goto_line_num = rmalloc(sizeof(int32_t));
+                    *goto_line_num = atoi(param);
+                    procedure[i].payload = goto_line_num;
+                } else if(!strcmp(instr, "call")) {
+                    procedure[i].instr = CONCEPT_CALL;
+                    if(!param_flag) exit(130);
+                    // perform an O(n) search to substitute in the actual position
+                    int32_t *call_addr;
+                    int flag = 0;
+                    for(int m = 0; m < procedure_call_table_length; m++) {
+                        if(!strcmp(param, procedure_call_table[m])) {
+                            // That's the procedure we want!
+                            call_addr = (int32_t *)rmalloc(sizeof(int32_t));
+                            procedure[i].payload = call_addr;
+                            flag = 1;
+                        }
+                    }
+                    if(!flag) { printf("Illegal call.\n"); exit(130); }
+                } else if(!strcmp(instr, "gstore")) {
+                    procedure[i].instr = CONCEPT_GSTORE;
+                    if(!param_flag) exit(130);
+                    int32_t *gstore_var = rmalloc(sizeof(int32_t));
+                    *gstore_var = atoi(param);
+
+                } else if(!strcmp(instr, "fstore")) {
+                    procedure[i].instr = CONCEPT_FSTORE;
+                    if(!param_flag) exit(130);
+
+                    float *f = rmalloc(sizeof(float));
+                    *f = (float)atof(param);
+                    procedure[i].payload = (void *)f;
+
+
+                } else if(!strcmp(instr, "fload")) procedure[i].instr = CONCEPT_FLOAD;
+                else if(!strcmp(instr, "gload")) procedure[i].instr = CONCEPT_GLOAD;
+                else exit(130); // ABRT
+            }
+
+            // if
+
+            compiled_bytecode_collection[procedure_counter] = procedure;
+            procedure_counter++;
+        }
+
+        return compiled_bytecode_collection;
+    }
+}
+
 
 void run(char *arg) {
     // read in the program
@@ -1046,8 +1184,8 @@ void run(char *arg) {
     ConceptStack_t f_stack;
     stack_alloc(&i_stack, (size_t)CONCEPTIP_MAX_LENGTH);
     stack_alloc(&f_stack, (size_t)CONCEPTFP_MAX_LENGTH); // TODO TODO TODO
-    ConceptBytecode_t **bytecodes = parse();
-    event_loop(bytecodes[0], &f_stack, &i_stack);
+    //ConceptBytecode_t **bytecodes = parse();
+    //event_loop(bytecodes[0], &f_stack, &i_stack);
     cleanup(&i_stack);
     cleanup(&f_stack);
 }
